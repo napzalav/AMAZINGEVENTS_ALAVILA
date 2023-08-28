0ccbac9d902a01eaf977ef134c5760fa077a1ed1
napzalav <cesaralavila@hotmail.com>
2023-08-28 01:09:55 -0300

    add loop home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,18 @@
 console.log("home probando probando...")
+
+let contenedorEventos = document.getElementById('contenedorEventos');
+
+for(let event of data.events){
+    let card = `<div class="card col-11 col-sm-4 col-md-3 col-xl-2">
+    <img src="${event.image}" class="card-img-top" alt="${event.name}">
+    <div class="card-body">
+        <h5 class="card-title">${event.name}</h5>
+        <p class="card-text">${event.description}</p>
+    </div>
+    <div class="card-footer">
+        <a href="#">$${event.price} USD</a>
+        <a href="./details.html" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>`;
+contenedorEventos.innerHTML += card;
+}
