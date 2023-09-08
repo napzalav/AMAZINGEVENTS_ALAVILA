951939b4d484bd24ab895b77f97aa09d6a9597e6
napzalav <cesaralavila@hotmail.com>
2023-09-08 01:11:51 -0300

    update details.js + style

--- assets/js/details.js
+++ assets/js/details.js
@@ -5,26 +5,36 @@ const id = new URLSearchParams(quearySearch).get("id");
 let eventito = data.events.find((evento) => evento._id == id);
 console.log(eventito);
 
+let concurrencia = '';
+if (eventito.estimate){
+    concurrencia = `<p><b>Estimate: </b>${eventito.estimate}</p>`;
+} else {
+    concurrencia = `<p><b>Assistance: </b>${eventito.assistance}</p>`;
+}
+
 function detailCard() {
     // console.log(objeto);
     document.getElementById("containerCard").innerHTML =
 `<div class="card">
-<div class="card-img">
-    <img src="${eventito.image}" class="card-img-top" alt="{eventito.name}">
-</div>
-<div class="info">
-    <div class="card-body">
-        <h2>${eventito.name}</h2>
-        <p>${eventito.category}</p>
-        <p>${eventito.description}</p>
-        <p>Capacity: ${eventito.capacity}</p>
-        <p>Place: ${eventito.place}</p>
+    <div class="card-img">
+        <img src="${eventito.image}" class="card-img-top" alt="{eventito.name}">
     </div>
-    <div class="card-footer">
-        <p>Price: $${eventito.price} USD</p>
-        <p>Date: ${eventito.date}</p>
+    <div class="info-details">
+        <div class="card-body">
+            <h2><b><i>${eventito.name}</i></b></h2>
+            <i>
+            <p><b>Category: </b>${eventito.category}</p>
+            <p><b>Place: </b>${eventito.place}</p>
+            <p><b>Capacity: </b>${eventito.capacity}</p>
+            ${concurrencia}
+            <p>${eventito.description}</p>
+            </i>
+        </div>
+        <div class="card-footer">
+            <p><b><i>Price: </i></b>$${eventito.price} USD</p>
+            <p><b><i>Date: </i></b>${eventito.date}</p>
+        </div>
     </div>
-</div>
 </div>`
 
 }
